097ef62770df8f954cc759edd8ceffc28bae9847
Kwane Smith <smithkwane6@gmail.com>
2020-02-19 07:18:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,28 +1,25 @@
 class Owner
   # code goes here
-  # code goes helper_method
-  attr_reader :species, :name
-  attr_accessor :cat, :dog
+end 
+  attr_reader :name, :species
+  attr_accessor :cats, :dogs
 
   @@all = []
-  @@cats = []
-  @@dogs = []
 
   def initialize(name)
-    @species = "human"
-    @@all << self
     @name = name
-    @dogs = []
+    @species = "human"
     @cats = []
+    @dogs = []
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
-
   def self.count
-    @@all.size
+    @@all.count
   end
 
   def self.reset_all
@@ -30,49 +27,36 @@ class Owner
   end
 
   def say_species
-    return "I am a human."
+    "I am a #{@species}."
   end
 
-  def cats
-    @cats
+  def buy_cat(cat)
+    Cat.new(cat,self)
   end
 
-  def dogs
-    @dogs
-  end
-
-  def buy_cat(name)
-    cat = Cat.new(name, self)
-    @@cats << cat
-    cat.owner = self
-  end
-
-  def buy_dog(name)
-    dog = Dog.new(name, self)
-    @@dogs << dog
-    dog.owner = self
+  def buy_dog(dog)
+    Dog.new(dog,self)
   end
 
   def walk_dogs
-    self.dogs.each {|dog| dog.mood = "happy"}
-
+    @dogs.each {|dog| dog.mood = "happy"}
   end
 
   def feed_cats
-    self.cats.each {|cat| cat.mood = "happy"}
+    @cats.each {|cat| cat.mood = "happy"}
   end
 
   def sell_pets
-    @pets.collect do |species, instances|
-      instances.each do |pet|
-        pet.mood = "nervous"
-      end
-      instances.clear
+    pets = @dogs+@cats
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
     end
+    @dogs = []
+    @cats = []
   end
 
   def list_pets
-    return "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
-
 end
