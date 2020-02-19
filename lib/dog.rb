99471841c69162b9bb34c83dcf87d339b41c16db
Kwane Smith <smithkwane6@gmail.com>
2020-02-19 07:06:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,27 @@
 class Dog
   # code goes here
-  attr_accessor :owner, :mood
   attr_reader :name
+  attr_accessor :mood, :owner
 
-  @@all = [ ]
+  @@all = [] 
+
 
   def initialize(name, owner)
-      @name = name
-      @owner = owner
-      @@all << self
-      @owner.pets=(self)
-      @mood = "nervous"
-  end
+    @name = name
+    @mood = 'nervous'
+    @owner = owner
+    @@all << self
+    self.owner = @owner
+    owner.dogs << self if !@owner.dogs.include?(self)
+  end 
 
-  def self.all
-      @@all
-  end
+   def self.all 
+    @@all
+  end 
+
+  def self.clear
+    @@all.clear
+  end 
+
 
 end
